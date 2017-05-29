ff1ae332d1ced20dcc70c01337f91d95838ad0ab
emlody <emlody@gmail.com>
2017-05-29 22:38:47 +0200

    2

---
+++ Lab2/src/lab2_zad1/Firma.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lab2_zad1;
+
+/**
+ * @author PRz
+ *
+ */
+public class Firma {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lab2/src/lab2_zad1/Pracownik.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lab2_zad1;
+
+/**
+ * @author PRz
+ *
+ */
+public class Pracownik {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
