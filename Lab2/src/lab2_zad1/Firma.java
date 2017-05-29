3c82e196afa0b18acf97015c23440d56788015b1
emlody <emlody@gmail.com>
2017-05-29 22:57:58 +0200

    3

--- Lab2/src/lab2_zad1/Firma.java
+++ Lab2/src/lab2_zad1/Firma.java
@@ -4,17 +4,33 @@
 package lab2_zad1;
 
 /**
- * @author PRz
+ * @author Piotr Rzezniczek 29322
  *
  */
 public class Firma {
+		
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Pracownik pracownik1 = new Pracownik();
+		Pracownik pracownik2 = new Pracownik();
+		Pracownik pracownik3 = new Pracownik();
+		
+		pracownik1.imie = "Janek" ;
+		pracownik1.nazwisko = "Kowalski" ;
+		pracownik1.rokUrodzenia = 1977 ;
+		pracownik1.stazPracy = 5;
+		
+		pracownik2.imie = "Marian" ;
+		pracownik2.nazwisko = "Bykowski" ;
+		pracownik2.rokUrodzenia = 1959 ;
+		pracownik2.stazPracy = 15;
+		
+		pracownik3.imie = "Tadeusz" ;
+		pracownik3.nazwisko = "Zarobiony" ;
+		pracownik3.rokUrodzenia = 1966 ;
+		pracownik3.stazPracy = 19;
+		
 	}
 
 }
--- Lab2/src/lab2_zad1/Pracownik.java
+++ Lab2/src/lab2_zad1/Pracownik.java
@@ -1,20 +1,17 @@
-/**
- * 
- */
 package lab2_zad1;
 
 /**
- * @author PRz
+ * @author Piotr Rzezniczek 29322
  *
+ *         Pracownik - klasa, która bêdzie przedstawia³a pracownika dowolnej
+ *         firmy. Podstawowe informacje o pracowniku jakie s¹ potrzebne
+ *         pracodawcy to imiê, nazwisko, rok urodzenia, sta¿ pracy
  */
 public class Pracownik {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	String imie;
+    String nazwisko;
+    int rokUrodzenia;
+    int stazPracy;
 
 }
